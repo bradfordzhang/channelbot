02007348dbe95c483c8420c3ebe838ba58815173
Bradford Zhang <zyc@zyc.name>
2022-11-17 16:17:48 +0800

    update commands
    Signed-off-by: Bradford Zhang <zyc@zyc.name>

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ```bash
 docker run -d --restart=always --env BOT_TOKEN="YOUT_TOKEN" --name="channelbot" ghcr.io/bradfordzhang/channelbot
 ```
+
+可选env:
+
+`DEBUG: true/false 默认false`
--- main.go
+++ main.go
@@ -20,7 +20,13 @@ func main() {
 		log.Panic(err)
 	}
 
-	bot.Debug = true
+	if os.Getenv("DEBUG") == "" {
+		bot.Debug = false
+		log.Println("Debug off")
+	} else {
+		bot.Debug = true
+		log.Println("Debug on")
+	}
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -30,33 +36,62 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil {
-			if strings.Contains(update.Message.Text, "官网") {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "网址： https://www.fishport.cyou\n点击右侧 \"Buy it now\" 即可")
-				msg.ReplyToMessageID = update.Message.MessageID
+		if update.Message == nil {
+			continue
+		}
 
-				bot.Send(msg)
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+			switch update.Message.Command() {
+			case "start":
+			case "about":
+				msg.Text = "I'm zyc's channel bot.\nSource code: https://github.com/bradfordzhang/channelbot/"
+			case "status":
+				msg.Text = "I'm ok!"
+			default:
+				msg.Text = "未知指令"
 			}
 
-			if strings.Contains(update.Message.Text, "截图") {
-				msg := tgbotapi.NewPhoto(update.Message.Chat.ID, tgbotapi.FileURL("https://raw.githubusercontent.com/bradfordzhang/res/main/%E6%88%AA%E5%9B%BE.jpg"))
-				msg.ReplyToMessageID = update.Message.MessageID
-
-				bot.Send(msg)
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
 			}
+			continue
+		}
 
-			if strings.Contains(update.Message.Text, "钓鱼") {
-				msg := tgbotapi.NewVideo(update.Message.Chat.ID, tgbotapi.FileURL("https://raw.githubusercontent.com/bradfordzhang/res/main/%E9%92%93%E9%B1%BC.mp4"))
-				msg.ReplyToMessageID = update.Message.MessageID
+		if strings.Contains(update.Message.Text, "官网") {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "网址： https://www.fishport.cyou\n点击右侧 \"Buy it now\" 即可")
+			msg.ReplyToMessageID = update.Message.MessageID
 
-				bot.Send(msg)
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
 			}
+		}
 
-			if strings.Contains(update.Message.Text, "机场") {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "自用机场推荐: https://s.zyc.name/AUIoQWV/")
-				msg.ReplyToMessageID = update.Message.MessageID
+		if strings.Contains(update.Message.Text, "截图") {
+			msg := tgbotapi.NewPhoto(update.Message.Chat.ID, tgbotapi.FileURL("https://raw.githubusercontent.com/bradfordzhang/res/main/%E6%88%AA%E5%9B%BE.jpg"))
+			msg.ReplyToMessageID = update.Message.MessageID
 
-				bot.Send(msg)
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+		}
+
+		if strings.Contains(update.Message.Text, "钓鱼") {
+			msg := tgbotapi.NewVideo(update.Message.Chat.ID, tgbotapi.FileURL("https://raw.githubusercontent.com/bradfordzhang/res/main/%E9%92%93%E9%B1%BC.mp4"))
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+		}
+
+		if strings.Contains(update.Message.Text, "机场") {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "自用机场推荐: https://s.zyc.name/AUIoQWV/")
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
 			}
 		}
 	}
